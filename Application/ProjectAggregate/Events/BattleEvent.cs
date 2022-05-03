625d0fde1d6021e099cc5ccdb11d2babb3651310
Edes0 <90324120+Edes0@users.noreply.github.com>
2022-05-03 14:25:50 +0200

    nytt

--- Application/ProjectAggregate/Events/BattleEvent.cs
+++ Application/ProjectAggregate/Events/BattleEvent.cs
@@ -4,6 +4,11 @@ namespace Application.ProjectAggregate.Events
 {
     public static class BattleEvent
     {
+        /// <summary>
+        /// Updates Games, Wins and Defeats for both hamsters after a battle.
+        /// </summary>
+        /// <param name="winner"></param>
+        /// <param name="loser"></param>
         public static void StatsUpdate(HamsterModel winner, HamsterModel loser)
         {
             winner.Games++;
